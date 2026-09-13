550015a7bf5c56b58eecc9cefc1ec6a8e5b164b0
mnajarro1966 <mnajarro1966@gmail.com>
2026-09-12 22:24:32 -0700

    Update Supabase configuration values

--- journey-config.js
+++ journey-config.js
@@ -1,5 +1,5 @@
 // PUBLIC configuration only. Never put service_role, sb_secret_, or passwords here.
 export const journeyConfig = Object.freeze({
-  supabaseUrl: '',
-  supabasePublishableKey: ''
+  supabaseUrl: 'https://numkzsenjpasnxtalgph.supabase.co',
+  supabasePublishableKey: 'sb_publishable_iuAG28e4Wsi5ss_tdGPdRw_Fg2koi6W'
 });
